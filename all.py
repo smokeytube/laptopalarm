6d87df2a4816de1aa5530c26e256dfb5aa316fbc
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-12-02 16:34:47 -0500

    Create all.py

---
+++ all.py
@@ -0,0 +1,44 @@
+import winsound
+import time
+import keyboard
+# import time
+# time.sleep(10)
+# winsound.Beep(17000, 500)
+
+while True:
+    if keyboard.is_pressed('e'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('h'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('w'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('f'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('b'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('c'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('v'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('m'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
+    elif keyboard.is_pressed('n'):
+        while True:
+            winsound.Beep(660, 1000)
+            time.sleep(1)
